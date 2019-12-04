c075723702b2fbe457eb89ae938525899450598e
Reutg <48221955+Reutg@users.noreply.github.com>
2019-12-04 14:52:00 +0200

    initial store setup

---
+++ src/stores/ImagesStore.js
@@ -0,0 +1,8 @@
+import { observable } from 'mobx'
+
+export class ImagesStore {
+    // @observable name
+    // @observable completed = falseconstructor(name) {
+    //           this.name = name
+    //       }
+      }
