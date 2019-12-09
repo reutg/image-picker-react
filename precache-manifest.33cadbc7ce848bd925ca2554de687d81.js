682aae8827f6a835ac6fbe08252515deb2e3e5c2
Reutg <48221955+Reutg@users.noreply.github.com>
2019-12-09 23:44:06 +0200

    Updates

---
+++ precache-manifest.33cadbc7ce848bd925ca2554de687d81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43db91ab5c69a05863e417be3928741b",
+    "url": "/image-picker-react/index.html"
+  },
+  {
+    "revision": "6f8f22929263a20e6e71",
+    "url": "/image-picker-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "32030799c58f0d2f1151",
+    "url": "/image-picker-react/static/js/2.7a2a0678.chunk.js"
+  },
+  {
+    "revision": "6f8f22929263a20e6e71",
+    "url": "/image-picker-react/static/js/main.a460c724.chunk.js"
+  },
+  {
+    "revision": "fb2867121d2f3b54af4d",
+    "url": "/image-picker-react/static/js/runtime-main.166f1be3.js"
+  }
+]);
